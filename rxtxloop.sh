33b319d72d5a8b0384bfa3223ba3a8a8f3592dde
PramoSan <pramodh2009@gmail.com>
2021-11-30 19:13:06 +0530

    File read and get input update

--- rxtxloop.sh
+++ rxtxloop.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
+
 sleep 1
 echo $(echo 'AT+I 0001<CR>' >  /dev/ttyACM2)
 echo $(echo 'AT+I 0001<CR>' >  /dev/ttyACM0)  
+
 sleep 1
 echo $(echo 'ATPFR=868000000<CR>' >  /dev/ttyACM2)
 echo $(echo 'ATPFR=868000000' >  /dev/ttyACM0)
+
 sleep 1
-echo $(echo 'ATPBM=1B<CR>' >  /dev/ttyACM2)
+##echo $(echo 'ATPBM=0<CR>' >  /dev/ttyACM2) ##text mode
+echo $(echo 'ATPBM=1B<CR>' >  /dev/ttyACM2) ##hex mode
+
 sleep 1
+echo "Enter File Name"
+read name
+file="$name"
+while read line; do
+echo $line
+
 
 valid=true
 c=1
-p=100
+p=5
 while [ $valid ]
 do
-sleep 0.2
+sleep 0.5
 echo $(echo 'AT+RX' >  /dev/ttyACM2)
-echo $(echo 'AT+TX Hello World Testing with RXTX-NP 868000000Hz' >  /dev/ttyACM0)
+echo $(echo "AT+TX $line" >  /dev/ttyACM0)
 ##(stty raw; cat > received.txt) < /dev/ttyACM0 >
 
 if [ $c -eq $p ];
@@ -25,5 +36,5 @@ break
 fi
 ((c++))
 done
-
+done < $file
 echo "Done Receiving $p packets"
